d4d364f442648d95fcbc55f857f74106356d1752
MultifruitV <100833471+MultifruitV@users.noreply.github.com>
2022-04-11 22:29:09 +0300

    Edit files and adaptation
    Rename eng.htm to index.html.
    Edit index.html (add in <html> lang="ru").
    Adaptation for mobile version.

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ document.getElementById('rus').onclick = function(){
 	window.location.href = 'rus.html';
 };
 document.getElementById('eng').onclick = function(){
-	window.location.href = 'eng.html';
+	window.location.href = 'index.html';
 };
